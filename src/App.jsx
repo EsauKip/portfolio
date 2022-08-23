192d6dd3e2c17ae0717b15cd35e3673c36b7ee45
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-23 15:36:20 +0300

    add css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/work/services'
+import Portfolio from './components/portfolio/portfolio'
+import Footer from './components/footer/footer'
+import Contact from './components/contact/contact'
 
 const App = () => {
   return (
     <>
-    
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Footer/>
+        <Contact/>
     </>
   )
 }
