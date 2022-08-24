d8c993ddeb13711f7c8fff9802ecca6080db3105
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-24 15:02:15 +0300

    add routes to the components

--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -6,9 +6,9 @@ const headerSocials = () => {
   return (
    
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
-        <a href="https://dribble.com" target="_blank"><SiFigma/></a>
+        <a href="https://www.linkedin.com/in/esaukiprono/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/EsauKip" target="_blank"><BsGithub/></a>
+        <a href="https://www.figma.com/files/user/1037780111482601144?fuid=1037780111482601144" target="_blank"><SiFigma/></a>
     </div>
   )
 }
