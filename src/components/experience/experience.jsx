8cd7b98f024c158411079c5352a9a018c17bd332
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-24 20:41:08 +0300

    create experiences

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -3,7 +3,18 @@ import './experience.css'
 
 const experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+
+    <div className="container experience__container">
+    
+    <div className="experience__frontend">
+    </div>
+    <div className="experience__backend">
+    </div>
+
+    </div>
+
+    </section>
   )
 }
 
