bca9b871811fa1300f4633c554fb6a03265cb460
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-25 13:07:55 +0300

    add styles to the icons

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -14,32 +14,32 @@ const experience = () => {
       <h3>Frontend Development</h3>
         <div className="experience__content">
         <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div>  <h4>HTML</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>CSS</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div>  <h4>JavaScript</h4>
           <small className="text-light">Intermediate</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div> <h4>Angular</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>Bootstrap</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div> <h4>React</h4>
           <small className="text-light">Intermediate</small></div>
           </article>
@@ -50,42 +50,42 @@ const experience = () => {
       <h3>Backend Development</h3>
         <div className="experience__content">
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>Flask</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>Django</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div>  <h4>SQL</h4>
           <small className="text-light">Intermediate</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>PosgreSQL</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>REST API</h4>
           <small className="text-light">Experienced</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div> <h4>PHP</h4>
           <small className="text-light">Basic</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
           <div><h4>MYSQL</h4>
           <small className="text-light">Basic</small></div>
           </article>
           <article className='experience__details'>
-          <BsPatchCheckFill/>
+          <BsPatchCheckFill className='icon'/>
         <div> <h4>Python</h4>
           <small className="text-light">Experienced</small></div>
           </article>
